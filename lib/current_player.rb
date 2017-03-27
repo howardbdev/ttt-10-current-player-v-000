b9e48ffa0479421f7ccf62eb4b2530c3520dc546
Howard DeVennish <howarddevennish@gmail.com>
2017-03-27 21:18:23 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,28 @@
+# how many turns have been played
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      counter += 1
+    end
+  end
+   return counter
+end
+
+
+# MINE!!!
+# current player
+# def current_player(board)
+#   if turn_count(board) % 2 == 0
+#     return "X"
+#     else
+#     return "O"
+#   end
+# 
+# end
+
+# Pat's solution
+# current player
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
